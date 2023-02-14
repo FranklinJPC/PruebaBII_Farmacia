8c0127e7dc11c0122d81db1f100c5c48e1a4e7b9
Franklin <franklin.patino@epn.edu.ec>
2023-02-14 10:27:51 -0500

    Creacion - Form base - Main sin datos - Funcionalidades nulas

---
+++ src/Productos_ver.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Productos_ver {
+    private JPanel Principal;
+    private JPanel ventana_opciones;
+    private JButton crearButton;
+    private JButton buscarButton;
+    private JButton actualizarButton;
+    private JButton borrarButton;
+    private JTextField tf_codigo;
+    private JTextField td_producto;
+    private JTextField tf_compra;
+    private JComboBox cb_venta;
+    private JComboBox cb_proveedor;
+    private JTextField tf_stock;
+}
